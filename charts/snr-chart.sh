b02096da0be8732103e62162e172ec5126c3b088
Joe Desbonnet <jdesbonnet@gmail.com>
2016-02-03 18:10:18 +0000

    plot satellite SNR using heatmap also (more efficient)

--- charts/snr-chart.sh
+++ charts/snr-chart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 JAVA="java -cp /home/joe/workspace/GNSS_RTK/bin"
-$JAVA NMEAToSV < $1 > snr.dat
+$JAVA NMEAToSV -heatmap heatmap.dat < $1 > snr.dat
 
 gnuplot <<- EOF
   set terminal pngcairo size 1280,720 background rgb 'black'
@@ -30,4 +30,35 @@ gnuplot <<- EOF
   set palette model RGB defined ( 0 'blue', 50 'red', 80 'yellow', 99 'green' )
   plot 'snr.dat' using 4:3:5 with points pointtype 1  palette title ''
 EOF
+
+gnuplot <<- EOF
+
+  set terminal pngcairo size 1280,720 background rgb 'black'
+  set output "snr-heatmap.png"
+
+  set border lc rgb 'white'
+  set key tc rgb 'white'
+  set grid lc rgb 'white'
+  set style line 1 linecolor rgb "green"
+  set style line 2 linecolor rgb "blue"
+
+  set title "GNSS Signal-to-Noise Ratio as function of position in sky.\nStatic survey with u-Blox Neo-6M module. Limited sky view. Arbitrary SNR units (higher is better)." textcolor rgb "white"
+
+  set label "Joe Desbonnet, 2 Feb 2016" at graph -0.1,-0.06 font ",8" tc rgb "white"
+
+  set xlabel "Azimuth (degrees clockwise from north)" textcolor rgb "white"
+  set ylabel "Elevation (degrees)" textcolor rgb "white"
+  #set y2label "test" textcolor rgb "white"
+
+  set xrange [0:360]
+  set yrange [0:90]
+
+  set xtics 45
+  set ytics 15
+
+  set palette model RGB defined ( 0 'black', 10 'blue', 50 'red', 80 'yellow', 99 'green' )
+  plot 'heatmap.dat' using 1:2:3 with image
+
+EOF
+
 
