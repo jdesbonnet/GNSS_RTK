10b5324f52ebe20f4fc5cedda9de8e3822ad965e
Joe Desbonnet <jdesbonnet@gmail.com>
2017-01-14 17:44:53 +0000

    small changes to charts

--- charts/latlngheatmap.gp
+++ charts/latlngheatmap.gp
@@ -21,7 +21,7 @@
   set style line 1 linecolor rgb "green"
   set style line 2 linecolor rgb "blue"
 
-set title "Heatmap histogram of 2 weeks of static GPS survey. u-Blox Neo-6M module. Limited sky view." textcolor rgb 'white'
+set title "Heatmap histogram of 3 months of static GPS survey. u-Blox Neo-6M module. Limited sky view." textcolor rgb 'white'
 set xlabel "Longitude (m / °)" textcolor rgb 'white'
 set ylabel "Latitude (m / °)" textcolor rgb 'white'
 
@@ -41,7 +41,7 @@ set yrange [lat_to_meters(53.2824,clat):lat_to_meters(53.2828,clat)]
 set ytics nomirror
 set y2tics	
 
-set label "Joe Desbonnet, 10 Feb 2016" at graph -0.06,-0.08 font "8" tc rgb "white"
+set label "Joe Desbonnet, 10 May 2016" at graph -0.06,-0.08 font "8" tc rgb "white"
 
 set palette model RGB defined ( 0 'black', 10 'blue', 50 'red', 80 'yellow', 99 'white' )
 plot 'latlngheatmap.dat' using (lng_to_meters($2,clng,clat)):(lat_to_meters($1,clat)):3 with image
--- charts/snr-chart.sh
+++ charts/snr-chart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 JAVA="java -cp /home/joe/workspace/GNSS_RTK/bin"
-$JAVA NMEAToSV -heatmap heatmap.dat < $1 > snr.dat
+NMEA_IN=$1
+
+$JAVA NMEAToSV -heatmap heatmap.dat < $NMEA_IN > snr.dat
 
 gnuplot <<- EOF
   set terminal pngcairo size 1280,720 background rgb 'black'
@@ -14,7 +16,7 @@ gnuplot <<- EOF
 
   set title "GPS satellite signal-to-noise ratio (SNR) as function of position in sky.\nStatic survey with u-Blox Neo-6M module. Limited sky view. Arbitrary SNR units (higher is better)." textcolor rgb "white"
 
-  set label "Joe Desbonnet, 8 Feb 2016" at graph -0.01,-0.05 font "8" tc rgb "white"
+  set label "Joe Desbonnet, 10 May 2016" at graph -0.01,-0.05 font "8" tc rgb "white"
 
   set xlabel "Azimuth (degrees clockwise from north)" textcolor rgb "white"
   set ylabel "Elevation (degrees)" textcolor rgb "white"
@@ -46,7 +48,7 @@ gnuplot <<- EOF
   set title "GPS satellite signal-to-noise ratio (SNR) as function of position in sky.\nStatic survey with u-Blox Neo-6M module. Limited sky view. Arbitrary SNR units (higher is better)." textcolor rgb "white"
 
 
-  set label "Joe Desbonnet, 8 Feb 2016" at graph -0.04,-0.08 font "8" tc rgb "white"
+  set label "Joe Desbonnet, 10 May 2016" at graph -0.04,-0.08 font "8" tc rgb "white"
   set label "SNR" at graph 1.03,1.05 font "8" tc rgb "white"
 
   set xlabel "Azimuth (degrees clockwise from north)" textcolor rgb "white"
@@ -60,7 +62,8 @@ gnuplot <<- EOF
   set ytics 15
 
   set palette model RGB defined ( 0 'black', 10 'blue', 50 'red', 80 'yellow', 99 'green' )
-  plot 'heatmap.dat' using 1:2:3 with image
+  plot 'heatmap.dat' using 1:2:3 with image, \
+    'svpeak2.dat' using 3:2:1 with labels center textcolor rgb 'white'
 
 EOF
 
