4236dd36a19845622c2e9acd7821cd7f9cea68fc
Joe Desbonnet <jdesbonnet@gmail.com>
2018-10-03 21:55:50 +0000

    no comment

--- ublox_m8p/start_base_station.sh
+++ ublox_m8p/start_base_station.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+TS=`date +%Y%m%d-%H%M`
 STR2STR=str2str
-DEVICE=$1
-${STR2STR} -in serial://${DEVICE}:230400#ubx -out tcpsvr://:22101 -c ./cfg_m8p_basestation.cmd
+DEVICE=ttyACM0
+${STR2STR} -in serial://${DEVICE}:230400#ubx -out tcpsvr://:21101 -c ./cfg_m8p_basestation.cmd &
+sleep 1
+netcat localhost 21101 > base-${TS}.ubx 
 
