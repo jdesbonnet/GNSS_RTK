f687dfe61ac6d81f196c5567f5bd27ff1bad42db
Joe Desbonnet <jdesbonnet@gmail.com>
2019-04-11 14:22:52 +0100

    no comment

--- src/ubx/ubx_cfg_tmode3.c
+++ src/ubx/ubx_cfg_tmode3.c
@@ -13,59 +13,86 @@
  * @author Joe Desbonnet
  */
 
+
+	
 // UBX-CFG-TMODE3 payload structure. 
 typedef struct {
 	uint8_t version;
-	uint8_t reserved0;	
-	uint16_t flags;
+	uint8_t reserved1;
+	
+	/* bits 0:7 mode (0 = disabled, 1 = survey in, 2 = position supplied); 
+	 * bit 8: pos in lat,lon,alt, else ECEF X,Y,Z used */
+	uint16_t flags;   
 
 
 	int32_t ecefXOrLat;
 	int32_t ecefYOrLon;
 	int32_t ecefZOrAlt;
+
+
+	int8_t ecefXOrLatHP;
+	int8_t ecefYOrLonHP;
+	int8_t ecefZOrAltHP;
+	uint8_t reserved2;
+
+	uint32_t fixedPosAcc; /* fixed position 3D accuracy 0.1mm units */
+	uint32_t svinMinDur;  /* Survey-in minimum duration seconds */
+	uint32_t svinAccLimit; 
+
+
+	uint8_t reserved3[8];
+
 } cfg_tmode3_t;
+
+uint32_t ubx_checksum (uint8_t payload[], int length) {
+	int i;
+	uint32_t A=0;
+	uint32_t B=0;
+
+	for (i = 0; i < length; i++) {
+		A += payload[i];
+		B += A;
+	}
+
+	A &= 0xff;
+	B &= 0xff;
+
+	return ((A<<8)|B);
+}
 
 int main (int argc, char **argv) {
 	int i, len, c, prevc;
 
-	int hexout = 0;
-	for (i = 0; i < argc; i++) {
-		if (argv[i][0]=='-' && argv[i][1]=='v') {
-			hexout=1;
-		}
+	// Initialize all zero
+	cfg_tmode3_t tmode3 = {0};
+
+	tmode3.flags = 1;
+	tmode3.svinMinDur = 128;
+	tmode3.svinAccLimit = 16*10000;
+
+
+
+
+
+
+	struct {
+		uint8_t msg_class;
+		uint8_t msg_id;
+		uint16_t length;
+		cfg_tmode3_t payload;
+	} ubx_frame;
+	ubx_frame.msg_class = 0x06;
+	ubx_frame.msg_id    = 0x71;
+	ubx_frame.length = sizeof(tmode3);
+	ubx_frame.payload = tmode3;
+
+	uint8_t *buf = (uint8_t *)&ubx_frame;
+
+	uint32_t checksum = ubx_checksum(buf, sizeof(ubx_frame));
+
+	for (int i = 0; i < sizeof(ubx_frame); i++) {
+		fprintf (stdout, "%02x ", buf[i]);
 	}
-
-	// Shift register to look for start of UBX-NAV-SOL pattern
-	uint32_t shift=0;
-
-
-	nav_svin_t navsvin;
-
-
-	while (!feof(stdin)) {
-		c = fgetc(stdin) & 0xff;
-
-		shift<<=8;
-		shift |= c;
-
-		// Check for start of UBX-NAV-SVIN message
-		if (shift == 0xB562013B) {
-
-			// Skip 2 byte length field
-			c = fgetc(stdin);
-			c = fgetc(stdin);
-
-			// Read NAV-SOL payload
-			fread (&navsvin, sizeof(nav_svin_t), 1,stdin);
-
-
-			fprintf (stdout, "t=%u  dur=%d  x=%d y=%d z=%d  acc=%d obs=%d valid=%d active=%d\n", 
-				navsvin.iTOW, 
-				navsvin.dur,
-				navsvin.meanX, navsvin.meanY, navsvin.meanZ,
-				navsvin.meanAcc, navsvin.obs, navsvin.valid,navsvin.active);
-		}
-		
-	}
-
+	fprintf (stdout, "checksum=%x",checksum);
+	fprintf (stdout,"\n");
 }
