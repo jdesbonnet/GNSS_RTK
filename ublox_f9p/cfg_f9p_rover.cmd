035868b733aae01611fb8d7dfc01b95dc7437254
Joe Desbonnet <jdesbonnet@gmail.com>
2019-04-15 17:47:37 +0000

    remove NAV-SVIN from rover cfg

--- ublox_f9p/cfg_f9p_rover.cmd
+++ ublox_f9p/cfg_f9p_rover.cmd
@@ -19,9 +19,6 @@
 # turn on UBX TIM TM2 messages on USB
 !UBX CFG-MSG 13 3 0 0 0 1 0 0
 
-# turn on UBX NAV-SVIN (survey in data) on USB
-!UBX CFG-MSG  1 59  0 0 0 1 0 0
-
 # turn on UBX NAV-SVINFO on USB
 !UBX CFG-MSG  1 48  0 0 0 1 0 0
 
