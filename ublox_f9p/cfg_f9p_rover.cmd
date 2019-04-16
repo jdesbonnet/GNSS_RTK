4f8bc9ee912fbde428daa6722dbb38e23d46359f
Joe Desbonnet <jdesbonnet@gmail.com>
2019-04-16 13:01:14 +0000

    use NAV-PVT instead of NAV-SOL

--- ublox_f9p/cfg_f9p_rover.cmd
+++ ublox_f9p/cfg_f9p_rover.cmd
@@ -19,14 +19,11 @@
 # turn on UBX TIM TM2 messages on USB
 !UBX CFG-MSG 13 3 0 0 0 1 0 0
 
-# turn on UBX NAV-SVIN (survey in data) on USB
-!UBX CFG-MSG  1 59  0 0 0 1 0 0
-
 # turn on UBX NAV-SVINFO on USB
 !UBX CFG-MSG  1 48  0 0 0 1 0 0
 
-# turn on UBX NAV-SOL on USB
-!UBX CFG-MSG  1  6  0 0 0 1 0 0
+# turn on UBX NAV-PVT on USB
+!UBX CFG-MSG  1  7  0 0 0 1 0 0
 
 # Set sample rate 4Hz
 !UBX CFG-RATE 250 1 1
