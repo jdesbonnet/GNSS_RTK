5579f58d73327d87a06d8b343d304c0803db1c33
Joe Desbonnet <jdesbonnet@gmail.com>
2019-04-16 13:33:29 +0000

    no comment

--- ublox_m8n/start_m8n.sh
+++ ublox_m8n/start_m8n.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-str2str -in serial://ttyUSB0:115200#ubx -out tcpsvr://:21102 -c ./cfg_m8n_raw.cmd
+#str2str -in serial://ttyUSB0:9600#ubx -out tcpsvr://:21103 -c ./cfg_m8n_raw.cmd
+str2str -in serial://ttyUSB0:115200#ubx -out tcpsvr://:21103 -c ./cfg_m8n_raw.cmd
