a193f668ee15c1cf67689212c275b2d871eb6755
Joe Desbonnet <jdesbonnet@gmail.com>
2019-05-14 11:46:14 +0000

    no comment

--- ublox_f9p/start_base_station.sh
+++ ublox_f9p/start_base_station.sh
@@ -2,8 +2,10 @@
 TS=`date +%Y%m%d-%H%M`
 STR2STR=str2str
 DEVICE=ttyACM0
+SURVEY_ACCURACY_M=10
+SURVEY_MIN_TIME_S=120
 
-../src/ubx/ubx_cfg_tmode3 120 16 -b > /dev/ttyACM0
+../src/ubx/ubx_cfg_tmode3 $SURVEY_MIN_TIME_S $SURVEY_ACCURACY_M -b > /dev/ttyACM0
 
 echo "Starting raw data streaming on port 21102"
 ${STR2STR} -in serial://${DEVICE}:230400#ubx -out tcpsvr://:21102 -c ./cfg_f9p_basestation.cmd >& base-${TS}.log &
