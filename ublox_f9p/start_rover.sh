2d2319d1c653c79a8b17aa8589d615c61eb3c3bd
Joe Desbonnet <jdesbonnet@gmail.com>
2019-10-05 17:40:47 +0000

    socket for PVT messages

--- ublox_f9p/start_rover.sh
+++ ublox_f9p/start_rover.sh
@@ -7,7 +7,7 @@ touch rover-started-${TS}.log
 
 rm -f rover-script-starting-* rover-tcm-in-* rover-tcpsrv-* rover-blink-*
 
-BASE=10.7.0.42
+BASE=10.7.0.54
 DEVICE=/dev/ttyACM0
 
 # Read RTCM from base station and send to GNSS receiver
@@ -17,6 +17,8 @@ sleep 2
 # Make roving GNSS data available 
 ${STR2STR} -in serial://ttyACM0:230400 -out tcpsvr://:21102 -c ./cfg_f9p_rover.cmd >& rover-tcpsrv-${TS}.log &
 sleep 2
+
+netcat localhost 21102 |  ../src/ubx/ubx_nav_pvt | ${STR2STR} -out tcpsvr://:21103 rover-PVT-out-${TS}.log &
 
 # Channel rover NMEA to Raspberry Pi Sensehat LED blinker
 #netcat localhost 21102 | /home/pi/GNSS_RTK/ublox_m8p/sense_hat_indicator >& rover-blink-${TS}.log &
