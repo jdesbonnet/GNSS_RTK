e0b7b5846e7c5d30fc65e12e730997f58da671b2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-16 06:55:02 +0800

    Delete ublox_m8p directory

--- ublox_m8p/rover_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Make rover data available on TCP server. 
-#
-DEVICE=$1
-PORT=$2
-str2str -in file://${DEVICE} -out tcpsvr://:${PORT}
-
--- ublox_m8p/start_rover_logging.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-STR2STR=str2str
-${STR2STR} -in serial://ttyACM0:230400#ubx -out rover.ubx -c ./cfg_m8p_raw_nonmea.cmd
-
