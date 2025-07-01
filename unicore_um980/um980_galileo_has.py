ec3b263b4ec4a6bfaad214ca62c8d6a32e5c339e
Joe Desbonnet <jdesbonnet@gmail.com>
2025-07-01 18:20:29 +0100

    update to um980_galileo_has.py

--- unicore_um980/um980_galileo_has.py
+++ unicore_um980/um980_galileo_has.py
@@ -9,7 +9,7 @@ def send_command (ser, cmd) :
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="Quectel GNSS")
+    parser = argparse.ArgumentParser(description="Unicore UM980 GNSS")
     parser.add_argument("--device", help="Serial port device", required=True)
     parser.add_argument("--baudrate", help="Serial port speed (bps/baud rate)", required=True)
 
@@ -20,16 +20,22 @@ if __name__ == '__main__':
     ser = serial.Serial(port=args.device, baudrate=baudrate, timeout=1)
 
 
-    send_command(ser,"version")
-    send_command(ser,"freset")
-    time.sleep(10)
+    #send_command(ser,"version")
+    #send_command(ser,"freset")
+    #time.sleep(10)
     send_command(ser,"CONFIG SIGNALGROUP 2")
-    send_command(ser,"GNGGA 0.2")
-    send_command(ser,"GNGSV 1")
-    send_command(ser,"GPRMC 1")
+
+    time.sleep(10)
+
     send_command(ser,"CONFIG PPP ENABLE E6-HAS")
     send_command(ser,"CONFIG PPP DATUM WGS84")
     send_command(ser,"CONFIG PPP CONVERGE 50 50")
+
+    time.sleep(10)
+
+    send_command(ser,"GNGGA 0.2")
+    send_command(ser,"GNGSV 1")
+    send_command(ser,"GNRMC 1")
 
 
 
